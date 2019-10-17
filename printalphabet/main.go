47f5853c2ef203fb4b624b1cba2116099d499b3c
AsemSkakova <asemaskakova@gmail.com>
2019-10-17 16:46:40 +0600

    Edited

---
+++ printalphabet/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+
+	for i := 0; i < 26; i++ {
+		z01.PrintRune(rune(97 + i))
+	}
+	z01.PrintRune('\n')
+}
---
+++ printdigits/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	i := 48
+	for i < 58 {
+		z01.PrintRune(rune(i))
+		i++
+	}
+	z01.PrintRune(10)
+}
